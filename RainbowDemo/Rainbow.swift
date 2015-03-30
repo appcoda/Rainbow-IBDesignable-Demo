5ce7fb1593a8df77b30c844a906a4c640e73f2c8
Simon Ng <simon@Simons-MacBook-Pro.local>
2015-03-30 21:08:39 +0800

    Revised the IBInspectable properties

--- RainbowDemo/Rainbow.swift
+++ RainbowDemo/Rainbow.swift
@@ -12,41 +12,13 @@ import UIKit
 
 @IBDesignable
 class Rainbow: UIView {
-    @IBInspectable var firstColor: UIColor = UIColor(red: (37.0/255.0), green: (252.0/255), blue: (244.0/255.0), alpha: 1.0) {
-        didSet {
-            self.setNeedsDisplay()
-        }
-    }
+    @IBInspectable var firstColor: UIColor = UIColor(red: (37.0/255.0), green: (252.0/255), blue: (244.0/255.0), alpha: 1.0)
+    @IBInspectable var secondColor: UIColor = UIColor(red: (171.0/255.0), green: (250.0/255), blue: (81.0/255.0), alpha: 1.0)
+    @IBInspectable var thirdColor: UIColor = UIColor(red: (238.0/255.0), green: (32.0/255), blue: (53.0/255.0), alpha: 1.0)
     
-    @IBInspectable var secondColor: UIColor = UIColor(red: (171.0/255.0), green: (250.0/255), blue: (81.0/255.0), alpha: 1.0) {
-        didSet {
-            self.setNeedsDisplay()
-        }
-    }
-    
-    @IBInspectable var thirdColor: UIColor = UIColor(red: (238.0/255.0), green: (32.0/255), blue: (53.0/255.0), alpha: 1.0) {
-        didSet {
-            self.setNeedsDisplay()
-        }
-    }
-    
-    @IBInspectable var innerCircleRadius:CGFloat = 80 {
-        didSet {
-            self.setNeedsDisplay()
-        }
-    }
-    
-    @IBInspectable var middleCircleRadius:CGFloat = 150 {
-        didSet {
-            self.setNeedsDisplay()
-        }
-    }
-    
-    @IBInspectable var outerCircleRadius:CGFloat = 215 {
-        didSet {
-            self.setNeedsDisplay()
-        }
-    }
+    @IBInspectable var innerCircleRadius:CGFloat = 80
+    @IBInspectable var middleCircleRadius:CGFloat = 150
+    @IBInspectable var outerCircleRadius:CGFloat = 215
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
